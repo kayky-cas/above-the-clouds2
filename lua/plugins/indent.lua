846ca3ffa43c20bc99544a174ed542580fa652c9
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-01-04 17:37:10 -0300

    Now its time

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,22 +1,22 @@
 return {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    config = function()
-        require("ibl").setup({
-            debounce = 100,
-            indent = { char = "|" },
-            whitespace = {
-                highlight = { "Whitespace", "NonText" },
-                remove_blankline_trail = true
-            },
-            scope = {
-                enabled = true,
-                show_end = true,
-                injected_languages = true
-            }
-        })
-    end
+	-- Add indentation guides even on blank lines
+	"lukas-reineke/indent-blankline.nvim",
+	-- Enable `lukas-reineke/indent-blankline.nvim`
+	-- See `:help ibl`
+	main = "ibl",
+	config = function()
+		require("ibl").setup({
+			debounce = 100,
+			indent = { char = "|" },
+			whitespace = {
+				highlight = { "Whitespace", "NonText" },
+				remove_blankline_trail = true,
+			},
+			scope = {
+				enabled = true,
+				show_end = true,
+				injected_languages = true,
+			},
+		})
+	end,
 }
