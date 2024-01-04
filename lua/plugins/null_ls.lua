d6283179350839b01a8b28b2174470ee55834ba4
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-01-04 17:29:19 -0300

    i think its gone

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,30 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        local builtins = null_ls.builtins
+
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+        null_ls.setup({
+            sources = {
+                builtins.formatting.stylua,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
+}
