d870f0e639fe4490fe2618bf75b96faad42b023f
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-01-06 12:31:13 -0300

    feat: onedark theme on lualine

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,15 +1,15 @@
 return {
-    "navarasu/onedark.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-        local opts = {
-            style = "darker",
-            transparent = true,
-        }
+	"navarasu/onedark.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		local opts = {
+			style = "darker",
+			transparent = true,
+		}
 
-        require("onedark").setup(opts)
+		require("onedark").setup(opts)
 
-        vim.cmd([[colorscheme onedark]])
-    end,
+		vim.cmd([[colorscheme onedark]])
+	end,
 }
