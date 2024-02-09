0c62b076d6b8f69e9383909db23404fcb00d6e8c
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-02-09 17:16:08 -0300

    Disable workspace keymap

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -21,7 +21,7 @@ local on_attach = function(_, bufnr)
     nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+    -- nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
