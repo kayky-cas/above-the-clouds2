80e219c2f7ab66c7c2854104bcce93d86c319b92
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-02-12 13:27:18 -0300

    feat: todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,19 @@
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+
+    config = function()
+        require("todo-comments").setup({
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        })
+
+        vim.keymap.set("n", "<leader>td", "<Cmd>TodoTelescope<CR>", { silent = true })
+    end,
+}
